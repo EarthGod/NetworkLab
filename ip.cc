29ee83ae282db1571eb03e343bd7738a84574d65
EarthGod <lyh244013554@gmail.com>
2016-12-12 16:08:30 +0800

    IP TOTALLY WORKED FOR CREATENET3 & ROUTERPORT

--- ip.cc
+++ ip.cc
@@ -59,7 +59,7 @@ void ip::run_timer(Timer *timer){
 				if(this->adjacentMatrix[i][j] != INF && this->adjacentMatrix[i][j] != 0)
 					cnt++;
 	
-	    click_chatter("111-%x: BROADCASTING adjacentMatrix. PORTCOUNT: %d", this->ip_addr, PORTCOUNT);
+	    //click_chatter("111-%x: BROADCASTING adjacentMatrix. PORTCOUNT: %d", this->ip_addr, PORTCOUNT);
 	    for (int i = 1; i < PORTCOUNT; ++i){
 	    	WritablePacket *packet = Packet::make(NULL,sizeof(struct MyIPHeader)+12*cnt);
 	    	struct MyIPHeader *format = (struct MyIPHeader*) packet->data();
@@ -79,13 +79,13 @@ void ip::run_timer(Timer *timer){
 	    			}
 	    		}
 	    	}
-	    	click_chatter("%x: 111 pushing %d", this->ip_addr, i);
+	    	//click_chatter("%x: 111 pushing %d", this->ip_addr, i);
 	    	output(i).push(packet);
 	    }
 	}
 	else if(timer == &this->timer_hello){
 		timer_hello.reschedule_after_sec(4*TIMERPERIOD);
-	    click_chatter("222-%x: BROADCASTING HELLO. PORTCOUNT: %d", this->ip_addr, PORTCOUNT);
+	    //click_chatter("222-%x: BROADCASTING HELLO. PORTCOUNT: %d", this->ip_addr, PORTCOUNT);
 	    for(int i = 1; i < PORTCOUNT; ++i){
 	    	WritablePacket *packet = Packet::make(NULL, sizeof(struct MyIPHeader));
 	    	struct MyIPHeader *format = (struct MyIPHeader*) packet->data();
@@ -108,11 +108,11 @@ void ip::push(int port, Packet *packet) {
 	if(port == 0){
 		struct MyTCPHeader *tcpheader = (struct MyTCPHeader*)packet->data();
 		if (ipaddr2mat.find(tcpheader->dest_ip) == ipaddr2mat.end()){
-			click_chatter("%x: NOT FOUND IN ROUTER TABLE: %x; PORT: %d;", this->ip_addr, tcpheader->dest_ip, port);
+			//click_chatter("%x: NOT FOUND IN ROUTER TABLE: %x; PORT: %d;", this->ip_addr, tcpheader->dest_ip, port);
 			packet->kill();
 			return;
 		}
-		click_chatter("333-%x: Received packet from %x on port %d, type: %d. packet dest: %x.", this->ip_addr, tcpheader->source_ip, port,tcpheader->type,tcpheader->dest_ip);
+		//click_chatter("333-%x: Received packet from %x on port %d, type: %d. packet dest: %x.", this->ip_addr, tcpheader->source_ip, port,tcpheader->type,tcpheader->dest_ip);
 		//wrap
 		WritablePacket* newpacket = Packet::make(NULL,sizeof(struct MyIPHeader)+(tcpheader->size));
 	    struct MyIPHeader* format = (struct MyIPHeader*) newpacket->data();
@@ -124,14 +124,14 @@ void ip::push(int port, Packet *packet) {
 		char *data = (char*)(newpacket->data()+sizeof(struct MyIPHeader));
 		memcpy(data, tcpheader, tcpheader->size);
 		//send
-		click_chatter("444-%x: sending to other ip element! Port: %d", this->ip_addr, findport(ip_port_table[tcpheader->dest_ip]));
+		//click_chatter("444-%x: sending to other ip element! Port: %d", this->ip_addr, findport(ip_port_table[tcpheader->dest_ip]));
 		int port2out = findport(ipaddr2mat[tcpheader->dest_ip]);
 		if (port2out != -1)
 			output(port2out).push(newpacket);
 	}
 	else{
 		struct MyIPHeader *ipheader = (struct MyIPHeader*)packet->data();
-		click_chatter("555-%x: Received packet from %x on port %d, type: %d. packet dest: %x.", this->ip_addr, ipheader->source, port, ipheader->type, ipheader->destination);
+		//click_chatter("555-%x: Received packet from %x on port %d, type: %d. packet dest: %x.", this->ip_addr, ipheader->source, port, ipheader->type, ipheader->destination);
 		if (ipheader->type == HELLO){
 			//HELLO: return the topology this router know
 			WritablePacket* newpacket = Packet::make(NULL,sizeof(struct MyIPHeader));
@@ -141,7 +141,7 @@ void ip::push(int port, Packet *packet) {
 			format->destination = ipheader->source;
 			format->ttl = 1;
 			format->size = sizeof(struct MyIPHeader);
-			click_chatter("666-%x: sending to other ip element! Port: %d", this->ip_addr, port);
+			//click_chatter("666-%x: sending to other ip element! Port: %d", this->ip_addr, port);
 			output(port).push(newpacket);
 			return;
 		}
@@ -158,6 +158,7 @@ void ip::push(int port, Packet *packet) {
 			ipaddr2mat[ipheader->source] = ptcnt;
 			adjacentMatrix[0][ptcnt] = 1;
 			adjacentMatrix[ptcnt][0] = 1;
+			/*
 			click_chatter("777-%x: updated adjacentMatrix! printing:", this->ip_addr);
 			for (int i = 0; i <= ptcnt; ++i){
 				for (int j = 0; j <= ptcnt; ++j){
@@ -166,6 +167,7 @@ void ip::push(int port, Packet *packet) {
 					}
 				}
 			}
+			*/
 			packet->kill();
 			return;
 		}
@@ -177,15 +179,15 @@ void ip::push(int port, Packet *packet) {
 				uint32_t addr1 = *((uint32_t*)(data + 12*i));
 				uint32_t addr2 = *((uint32_t*)(data + 12*i + 4));
 				uint32_t dist = *((uint32_t*)(data + 12*i + 8));
-				click_chatter("adding: %x, %x: %d", addr1, addr2, dist);
+				//click_chatter("adding: %x, %x: %d", addr1, addr2, dist);
 				if(ipaddr2mat.find(addr1) == ipaddr2mat.end()){
-					click_chatter("NOT found addr1: %x!", addr1);
+					//click_chatter("NOT found addr1: %x!", addr1);
 					ptcnt++;
 					ipaddr2mat[addr1] = ptcnt;
 					mat2ipaddr[ptcnt] = addr1;
 				}
 				if(ipaddr2mat.find(addr2) == ipaddr2mat.end()){
-					click_chatter("NOT found addr2: %x!", addr2);
+					//click_chatter("NOT found addr2: %x!", addr2);
 					ptcnt++;
 					ipaddr2mat[addr2] = ptcnt;
 					mat2ipaddr[ptcnt] = addr2;
@@ -195,6 +197,7 @@ void ip::push(int port, Packet *packet) {
 					this->adjacentMatrix[ipaddr2mat[addr2]][ipaddr2mat[addr1]] = dist;
 				}
 			}
+			/*
 			click_chatter("%x: parse BROAD finished!! now printing matrix: ", this->ip_addr);
 			for (int i = 0; i <= ptcnt; ++i){
 				for (int j = 0; j <= ptcnt; ++j){
@@ -203,19 +206,20 @@ void ip::push(int port, Packet *packet) {
 					}
 				}
 			}
+			*/
 			dijkstra();
 			packet->kill();
 			return;
 		}
 		if (ipheader->destination == this->ip_addr){
 			//push to TCP through port 0;
-			click_chatter("999-%x: Forwarding to TCP", this->ip_addr);
+			//click_chatter("999-%x: Forwarding to TCP", this->ip_addr);
 			WritablePacket* newpacket = Packet::make(packet->data()+sizeof(MyIPHeader), ipheader->size - sizeof(MyIPHeader));
 			output(0).push(newpacket);
 			return;
 		}
 		if(ipaddr2mat.find(ipheader->destination) != ipaddr2mat.end()){
-			click_chatter("000-%x: Forwarding to other IP", this->ip_addr);
+			//click_chatter("000-%x: Forwarding to other IP", this->ip_addr);
 			ipheader->ttl--;
 			output(findport(ipaddr2mat[ipheader->destination])).push(packet);
 			return;
@@ -250,10 +254,12 @@ void ip::dijkstra(){
 				this->path[i] = minpos;
 			}
 	}
+	/*
 	click_chatter("%x: DIJKSTRA FINISHED! PRINTING PATH&DIST!", this->ip_addr);
 	click_chatter("path:");
 	for (int i = 0; i <= ptcnt; ++i)
 		click_chatter("ipaddr-%x, numbered-%d!!! dist: %d, path: %d", mat2ipaddr[i], i, dist[i], path[i]);
+	*/
 }
 
 int ip::findport(int ptnum){
--- testip_net3.click
+++ testip_net3.click
@@ -1,5 +1,21 @@
 require(library /home/comnetsii/elements/routerport.click);
 
+//path between ip1&ip4
+rp1 :: RouterPort(DEV veth1, IN_MAC 3e:a9:5f:81:9c:5b, OUT_MAC a2:bb:d3:ab:8f:f5);
+rp2 :: RouterPort(DEV veth2, IN_MAC a2:bb:d3:ab:8f:f5, OUT_MAC 3e:a9:5f:81:9c:5b);
+//path between ip4&ip5
+rp3 :: RouterPort(DEV veth3, IN_MAC 0a:65:70:78:0f:51, OUT_MAC 46:c8:b7:40:e9:50);
+rp4 :: RouterPort(DEV veth4, IN_MAC 46:c8:b7:40:e9:50, OUT_MAC 0a:65:70:78:0f:51);
+//path between ip5&ip2
+rp5 :: RouterPort(DEV veth5, IN_MAC 5a:e8:03:37:31:70, OUT_MAC da:bd:ae:e6:2c:95);
+rp6 :: RouterPort(DEV veth6, IN_MAC da:bd:ae:e6:2c:95, OUT_MAC 5a:e8:03:37:31:70);
+//path between ip4&ip6
+rp7 :: RouterPort(DEV veth7, IN_MAC 82:fe:1c:cd:97:dc, OUT_MAC fa:5e:dc:98:4f:a9);
+rp8 :: RouterPort(DEV veth8, IN_MAC fa:5e:dc:98:4f:a9, OUT_MAC 82:fe:1c:cd:97:dc);
+//path between ip6&ip3
+rp9 :: RouterPort(DEV veth9, IN_MAC 66:2f:8b:49:96:12, OUT_MAC 4e:11:c8:00:de:28);
+rp10 :: RouterPort(DEV veth10, IN_MAC 4e:11:c8:00:de:28, OUT_MAC 66:2f:8b:49:96:12);
+
 ip1 :: ip(IP_ADDR 1.1.1.1);
 ip2 :: ip(IP_ADDR 2.2.2.2);
 ip3 :: ip(IP_ADDR 3.3.3.3);
@@ -11,20 +27,33 @@ cl1 :: simipgen(1.1.1.1, 2.2.2.2);
 cl2 :: simipgen(2.2.2.2, 3.3.3.3);
 cl3 :: simipgen(3.3.3.3, 1.1.1.1);
 
-cl1->[0]ip1[1]->[1]ip4;
-ip1[0]->Print("1.1.1.1 GET:")->Discard();
-ip4[1]->[1]ip1;
-cl2->[0]ip2[1]->[1]ip5;
-ip2[0]->Print("2.2.2.2 GET:")->Discard();
-ip5[1]->[1]ip2;
-cl3->[0]ip3[1]->[1]ip6;
-ip3[0]->Print("3.3.3.3 GET:")->Discard();
-ip6[1]->[1]ip3;
+cl1->[0]ip1[1]->rp1;
+rp2->[1]ip4;
+ip1[0]->Print("CLIENT1-1.1.1.1 GET")->Discard();
+ip4[1]->rp2;
+rp1->[1]ip1;
 
-ip4[2]->[2]ip5;
-ip5[2]->[2]ip4;
-ip4[3]->[3]ip6;
-ip6[3]->[3]ip4;
+cl2->[0]ip2[1]->rp6;
+rp5->[1]ip5;
+ip2[0]->Print("CLIENT2-2.2.2.2 GET")->Discard();
+ip5[1]->rp5;
+rp6->[1]ip2;
+
+cl3->[0]ip3[1]->rp10;
+rp9->[1]ip6;
+ip3[0]->Print("CLIENT3-3.3.3.3 GET")->Discard();
+ip6[1]->rp9;
+rp10->[1]ip3;
+
+ip4[2]->rp3;
+rp4->[2]ip5;
+ip5[2]->rp4;
+rp3->[2]ip4;
+
+ip4[3]->rp7;
+rp8->[3]ip6;
+ip6[3]->rp8;
+rp7->[3]ip4;
 
 Idle()->[0]ip4[0]->Discard();
 Idle()->[0]ip5[0]->Discard();
